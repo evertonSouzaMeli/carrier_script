e005fa74bc8c93a2a852a8b543f479ad314d3b58
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-11-29 05:59:47 -0300

    feat: minor refact

--- driver/script.js
+++ driver/script.js
@@ -11,22 +11,54 @@ const init = async () => {
 
     //await export_to_json_file('driver', converted_sheet, './converted_sheet')
 
-    //let files = await find_by_name_and_extension('./converted_sheet', 'driver', 'json')
+    let files = await find_by_name_and_extension('./converted_sheet', 'driver', 'json')
 
-    //let latest_file = files[files.length - 1]
+    let latest_file = files[files.length - 1]
 
-    //let parsedData = await parse_data_to_javascript_object('./converted_sheet', latest_file)
+    let parsedData = await parse_data_to_javascript_object('./converted_sheet', latest_file)
 
-    //console.log(parsedData)
+    let x = await generate_divergent_json(parsedData)
 }
 
-const generate_divergent_json = async () => {
+const get_carrier_id_by_driver_id = async (driver_id) =>{
+    try{
+        console.log("Getting carrier id...")
+
+        let req = await axios.get(`https://internal-api.mercadolibre.com/logistics/drivers/${driver_id}`, {
+            params: {
+                'scope': 'prod'
+            }
+        })
+
+        return req.data.carrier_id
+    }catch (err){
+        throw err
+    }
+}
+
+
+const generate_divergent_json = async (drivers) => {
     console.log(`Checking for divergent drivers from the carrier ${1}`)
+
+    let carrier_id = await get_carrier_id_by_driver_id(drivers[0].id)
 
     axios.defaults.timeout = 5000;
 
-    let req = await axios.get()
+    let req = await axios.get('https://internal-api.mercadolibre.com/logistics/drivers/search', {
+        params: {
+            'carrier_id': carrier_id
+        }, headers: {
+            'X-With-Kyc-User-Id': true
+        }
+    })
 
+    let res = req.data
+
+    //comparar as duas listas e ver valores diferentes
+    let diff_between_list = res.filter(value => !drivers.includes(value.id));
+
+    console.log('LISTA')
+    console.log(diff_between_list)
 }
 
 const backup = async () => {
